b3fee2785cf844cca3a78346df68e7353e79609d
niqitosiq <nikitashtanko@yahoo.com>
2019-11-04 17:14:32 +0400

    Add webpack

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,13 +1,35 @@
 var gulp         = require('gulp'),
     config       = require('../config'),
     uglify		 = require("gulp-uglify"),
-    concat		 = require("gulp-concat");
+    concat		 = require("gulp-concat"),
+    babel		 = require("gulp-babel"),
+    webpack      = require('webpack'),
+    webpackStre  = require('webpack-stream');
 
 
 
 gulp.task('js', function (done) {
-	return gulp.src([config.src.js+'/jquery.js', config.src.js+'/libs/*.js', config.src.js+'/*.js'])
-		.pipe(concat('script.js'))
+	return gulp.src(
+		[
+			'src/js/app.js'
+	    ])
+		.pipe(webpackStre({
+            output: {
+                filename: 'app.js',
+            },
+            module: {
+                rules: [
+                  {
+                    test: /\.(js)$/,
+                    exclude: /(node_modules)/,
+                    loader: 'babel-loader',
+                    query: {
+                      presets: ['env']
+                    }
+                  }
+                ]
+            }
+        }))
         .pipe(uglify())
         .pipe(gulp.dest(config.dest.js+'/'));
 })
