8172fd3564bf82304766ba5d0c5c26f09e2ad307
niqitosiq <nikitashtanko@yahoo.com>
2019-11-13 19:34:38 +0400

    My favorite libraries and setup for starting develop

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,39 +1,46 @@
-var gulp         = require('gulp'),
-    config       = require('../config'),
-    uglify		 = require("gulp-uglify"),
-    concat		 = require("gulp-concat"),
-    babel		 = require("gulp-babel"),
-    webpack      = require('webpack'),
-    webpackStre  = require('webpack-stream');
+var gulp           = require('gulp'),
+    config         = require('../config'),
+    uglify         = require("gulp-uglify-es").default,
+    concat         = require("gulp-concat"),
+    babel          = require("gulp-babel"),
+    webpack        = require('webpack'),
+    webpackStream  = require('webpack-stream');
 
 
 
 gulp.task('js', function (done) {
-	return gulp.src(
-		[
-			'src/js/app.js'
-	    ])
-		.pipe(webpackStre({
+    return gulp.src(
+        [
+            'src/js/app.js'
+        ])
+        .pipe(webpackStream({
             output: {
                 filename: 'app.js',
             },
             module: {
                 rules: [
-                  {
-                    test: /\.(js)$/,
-                    exclude: /(node_modules)/,
-                    loader: 'babel-loader',
-                    query: {
-                      presets: ['env']
+                    {
+                        test: /\.(js)$/,
+                        exclude: /(node_modules)/,
+                        loader: 'babel-loader',
+                        query: {
+                          presets: ['@babel/preset-env']
+                        }
                     }
-                  }
-                ]
-            }
+               ]
+            },
+            plugins: [
+                new webpack.ProvidePlugin({
+                    $: 'jquery',
+                    jQuery: 'jquery',
+                    'window.jQuery': 'jquery'
+                })
+            ]
         }))
         .pipe(uglify())
         .pipe(gulp.dest(config.dest.js+'/'));
 })
-	
+    
 gulp.task('js:watch', function() {
     gulp.watch(config.src.js+'/*', gulp.parallel('js'));
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,16 @@
-import $ from 'jQuery';
+import FastClick from 'fastclick';
+import svg4everybody from 'svg4everybody';
+// Config libs for every one project
+/*
+import inView from 'in-view';
+import swiper from 'swiper';
+import fancyBox from '@fancyapps/fancybox';
+import onePageScroll from 'onepage-scroll';
+*/
+svg4everybody();
 
 $(document).ready(function(){
-	alert("a");
+	console.log("Document ready with love ❤");
+	FastClick.attach(document.body);
 })
  
